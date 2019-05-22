90c7fe837a38dbe2e1fce4f6c36b12104ba3c18c
Eunwoo <dmsdn960@gmail.com>
2019-05-22 16:57:14 +0900

    2 room chat

--- public/main.js
+++ public/main.js
@@ -1,20 +1,38 @@
 const _name = prompt("이름을 입력하세요");
 const socket = io();
-socket.emit('joinRoom', 0, _name);
-const form = document.querySelector('form');
-const chat = document.querySelector('.messages');
-form.onsubmit = e => {
+let roomNum = 0;
+const form1 = document.getElementById('room1');
+const chat1 = document.getElementById('messages1');
+const form2 = document.getElementById('room2');
+const chat2 = document.getElementById('messages2');
+form1.onclick = e => {
+    roomNum = 0;
+    socket.emit('joinRoom', roomNum, _name);
+};
+form1.onsubmit = e => {
     e.preventDefault();
-    const input = document.querySelector('.m');
+    const input = document.getElementById('m1');
     const text = input.value;
-    text == '' || socket.emit('chat message', 0, _name, text);
+    text == '' || socket.emit('chat message', roomNum, _name, text);
     input.value = '';
     return false;
 };
-socket.on('chat message', (num, name, msg) => {
+form2.onclick = e => {
+    roomNum = 1;
+    socket.emit('joinRoom', roomNum, _name);
+};
+form2.onsubmit = e => {
+    e.preventDefault();
+    const input = document.getElementById('m2');
+    const text = input.value;
+    text == '' || socket.emit('chat message', roomNum, _name, text);
+    input.value = '';
+    return false;
+};
+socket.on('chat message', (name, msg) => {
     const li = document.createElement("li");
-    li.appendChild(document.createTextNode(msg));
-    chat.appendChild(li);
-    chat.scrollTop = chat.offsetHeight;
+    li.appendChild(document.createTextNode(name + " : " + msg));
+    roomNum ? chat2.appendChild(li) : chat1.appendChild(li);
+    roomNum ? chat2.scrollTop = chat2.offsetHeight : chat1.scrollTop = chat1.offsetHeight;
 });
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,47 @@ const _name: string = prompt("이름을 입력하세요");
 
 const socket = io();
 
-socket.emit('joinRoom', 0, _name);
 
-const form: HTMLFormElement = (<HTMLFormElement>document.querySelector('form'));
-const chat: HTMLUListElement = (<HTMLUListElement>document.querySelector('.messages'));
+let roomNum: number = 0;
 
-form.onsubmit = e => {
+const form1: HTMLFormElement = (<HTMLFormElement>document.getElementById('room1'));
+const chat1: HTMLUListElement = (<HTMLUListElement>document.getElementById('messages1'));
+
+const form2: HTMLFormElement = (<HTMLFormElement>document.getElementById('room2'));
+const chat2: HTMLUListElement = (<HTMLUListElement>document.getElementById('messages2'));
+
+form1.onclick = e => {
+    roomNum = 0;
+    socket.emit('joinRoom', roomNum, _name);
+}
+
+form1.onsubmit = e => {
     e.preventDefault();
-    const input: HTMLInputElement = (<HTMLInputElement>document.querySelector('.m'));
+    const input: HTMLInputElement = (<HTMLInputElement>document.getElementById('m1'));
     const text: string = input.value;
-    text=='' || socket.emit('chat message', 0, _name, text);
+    text=='' || socket.emit('chat message', roomNum, _name, text);
+    input.value = '';
+    return false;
+};
+
+form2.onclick = e => {
+    roomNum = 1;
+    socket.emit('joinRoom', roomNum, _name);
+}
+
+form2.onsubmit = e => {
+    e.preventDefault();
+    const input: HTMLInputElement = (<HTMLInputElement>document.getElementById('m2'));
+    const text: string = input.value;
+    text=='' || socket.emit('chat message', roomNum, _name, text);
     input.value = '';
     return false;
 };
 
 
-socket.on('chat message', (num: number, name: string, msg: string) => {
+socket.on('chat message', (name:string, msg: string) => {
     const li: HTMLLIElement = (<HTMLLIElement>document.createElement("li"));
-    li.appendChild(document.createTextNode(msg));
-    chat.appendChild(li);
-    chat.scrollTop = chat.offsetHeight;
+    li.appendChild(document.createTextNode(name + " : " + msg));
+    roomNum ? chat2.appendChild(li) : chat1.appendChild(li);
+    roomNum ? chat2.scrollTop = chat2.offsetHeight : chat1.scrollTop = chat1.offsetHeight;
 });
